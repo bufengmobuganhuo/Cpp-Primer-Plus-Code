bdca14375fc09d1faba65c0b9e00e58cec0cb47e
yuzhang <18681820448@163.com>
2024-12-21 18:36:38 +0800

    update ignored files

---
+++ chapter2_4/sqrt.cpp
@@ -0,0 +1,5 @@
+//
+// Created by yuzhang on 2024/12/21.
+//
+
+#include "sqrt.h"
---
+++ chapter2_4/sqrt.h
@@ -0,0 +1,16 @@
+//
+// Created by yuzhang on 2024/12/21.
+//
+
+#ifndef SQRT_H
+#define SQRT_H
+
+
+
+class sqrt {
+
+};
+
+
+
+#endif //SQRT_H
